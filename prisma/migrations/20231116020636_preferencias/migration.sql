802afc8412f33b5297e5654496d361e12c169757
JayaneXavier <jayanexv@gmail.com>
2023-11-16 00:37:45 -0300

    feat: Adiciona ao banco a tabela de Preferencia

---
+++ prisma/migrations/20231116020636_preferencias/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Preferencia" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "areaDeInteresse1" TEXT,
+    "areaDeInteresse2" TEXT,
+    "areaDeInteresse3" TEXT,
+    "areaDeInteresse4" TEXT,
+    "nivelDeExperiencia" TEXT,
+    "idUsuario" TEXT NOT NULL
+);
